6fb9919ff32d6a77ab1125ba6e1103250f322f4e
nicolas9909 <nicolico99@hotmail.com>
2019-10-30 15:59:08 +0000

    adding the function printf

---
+++ _printf.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * _printf - Print formatted.
+ * @format: Variable.
+ *
+ * Return: Number of characters written.
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list list;
+	format_t type[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"i", print_number},
+		{"d", print_number},
+		{"u", print_u_number},
+		{"b", binary},
+		{"r", reverse},
+		{"o", octal},
+		{"x", hex},
+		{"X", hex},
+		{NULL, NULL}
+	};
+	int print;
+
+	if (!format)
+		return (-1);
+
+	va_start(list, format);
+	print = find_type(format, type, list);
+	va_end(list);
+	return (print);
+}
